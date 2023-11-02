ff93eb7e9c883c1a9d3bfd346f4006606f60467a
Pablo P Varela <pablo@pablopunk.com>
2023-11-02 11:38:54 +0100

    remove boilerplate code

--- plugin/unclutter.lua
+++ plugin/unclutter.lua
@@ -1,8 +1,3 @@
-if vim.fn.has "nvim-0.7.0" == 0 then
-  vim.api.nvim_err_writeln "unclutter requires at least nvim-0.7.0"
-  return
-end
-
 -- make sure this file is loaded only once
 if vim.g.loaded_unclutter == 1 then
   return
